8244fb20d2c9154d9bb17706f84f758e63317bff
abdelrahman <gadoua50@gmail.com>
2022-04-06 01:42:34 +0200

    add test to check the argument of the reverseString function to be only string

--- tests/reverse-string.test.js
+++ tests/reverse-string.test.js
@@ -1,5 +1,13 @@
 import reverseString from "../scripts/reverse-string";
 
+test("arguments to the reverseString should only be string", () => {
+  expect(reverseString(1324)).toBe(null);
+  expect(reverseString({ name: "ali" })).toBe(null);
+  expect(reverseString([1, 2])).toBe(null);
+  expect(reverseString()).toBe(null);
+});
+
+
 test('reversing the string (hello)', () => {
     expect(reverseString('hello')).toBe('olleh');
 });
