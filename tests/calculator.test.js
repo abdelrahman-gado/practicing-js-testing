0ec5f14b6d0c9f1312274e89c3232ac7b02480c3
abdelrahman <gadoua50@gmail.com>
2022-04-05 22:38:35 +0200

    add tests to the calculator module called calculator.test.js in tests directory

---
+++ tests/calculator.test.js
@@ -0,0 +1,31 @@
+import calculator from "../scripts/calculator";
+
+test('add two numbers', () => {
+    expect(calculator.add(2, 4)).toBe(6);
+    expect(calculator.add(30, 40)).toEqual(70);
+});
+
+
+test('substract two numbers', () => {
+    expect(calculator.substract(10, 5)).toBe(5);
+    expect(calculator.substract(20, 2)).toEqual(18);
+});
+
+test('Substracting two numbers where the first is the smallest', () => {
+    expect(calculator.substract(5, 10)).toBe(5);
+    expect(calculator.substract(2, 20)).toBe(18);
+});
+
+test('dividing two numbers', () => {
+    expect(calculator.divide(20, 10)).toBeCloseTo(2);
+    expect(calculator.divide(2, 4)).toBeCloseTo(0.5);
+});
+
+test('dividing by zero', () => {
+    expect(calculator.divide(5, 0)).toBe(null);
+});
+
+test('multiplying two numbers', () => {
+    expect(calculator.multiply(2, 3.5)).toBeCloseTo(7);
+    expect(calculator.multiply(2, 5)).toBeCloseTo(10);
+});
